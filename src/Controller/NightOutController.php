9b10fd3b59076fdf38c4ccfe2e3cee81c233e3c2
(J)AVA <91890643+Avalhea@users.noreply.github.com>
2022-02-11 15:01:07 +0100

    Ajout et enlever un user à une sortie sur l'index et le filtre

--- src/Controller/NightOutController.php
+++ src/Controller/NightOutController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Form\FilterNightOutType;
+
 use App\Entity\NightOut;
 use App\Form\NightOutType;
 use App\Repository\StateRepository;
 use App\Repository\CampusRepository;
 use App\Repository\NightOutRepository;
 use App\Repository\UserRepository;
-use PhpParser\Node\Expr\New_;
+use App\Service\AddRemoveNightOut;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,83 +19,72 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/nightout', name: 'night_out')]
 class NightOutController extends AbstractController
 {
-    #[Route('index', name: '_index')]
-    public function index(NightOutRepository $nightOutRepository): Response
-    {
-        $nightOutList = $nightOutRepository->selectAll();
-
-        return $this->render('main/index.html.twig',
-            compact('nightOutList')
-        );
-
-
-    }
-
     /** Fonction qui permet d'afficher une sortie selon différents filtres */
-    #[Route('/nightoutfilter', name: '_filter')]
+    #[Route('/nightoutfilter/{idNightOut}', name: '_filter', requirements: ['idNightOut' => '\d+'])]
     public function nightOutFilterCampus(
         NightOutRepository $nightOutRepository, CampusRepository $campusRepository,
-        Request            $request
+        Request            $request, AddRemoveNightOut $addRmv, $idNightOut = 0
     ): Response
     {
         $campusList = $campusRepository->findAll();
-        /** On ne chercher pas à faire une nouvelle insertion d'une NightOut, ici on s'en sert pour récupérer des
-         * champs, dans le but de filtrer ce qu'on affiche
-         */
-        $isOrganizer = $request->query->get('is_organizer');
-        $isParticipant = $request->query->get('is_participant');
-        $notParticipant = $request->query->get('not_participant');
-        dump("Nos variables :");
-        $campus = $request->query->get("filter_night_out_campus");
-        $nightOutName = $request->query->get("filter_night_out_name");
-        $startDate = $request->query->get("filter_night_out_startTime");
-        $endDate = $request->query->get("filter_night_out_endTime");
-        if (is_null($campus)&&is_null($isOrganizer)&&is_null($isParticipant)&&is_null($notParticipant)&&
-            is_null($nightOutName)){
+
+        if($idNightOut!==0){
+            $addRmv->exec($idNightOut);
             $nightOutList = $nightOutRepository->selectAll();
         } else {
-            // Si les champs de Date ne sont pas remplies, on les initialise à des dates qui ne perturbent pas notre
-            // filtre
-            if (empty($startDate)) {
-                $startDate = new \DateTime();
-                dump("On initialise startdate");
+            /** On ne chercher pas à faire une nouvelle insertion d'une NightOut, ici on s'en sert pour récupérer des
+             * champs, dans le but de filtrer ce qu'on affiche
+             */
+            $isOrganizer = $request->query->get('is_organizer');
+            $isParticipant = $request->query->get('is_participant');
+            $notParticipant = $request->query->get('not_participant');
+            $campus = $request->query->get("filter_night_out_campus");
+            $nightOutName = $request->query->get("filter_night_out_name");
+            $startDate = $request->query->get("filter_night_out_startTime");
+            $endDate = $request->query->get("filter_night_out_endTime");
+            if (is_null($campus) && is_null($isOrganizer) && is_null($isParticipant) && is_null($notParticipant) &&
+                is_null($nightOutName)) {
+                $nightOutList = $nightOutRepository->selectAll();
+            } else {
+                // Si les champs de Date ne sont pas remplies, on les initialise à des dates qui ne perturbent pas notre
+                // filtre
+                if (empty($startDate)) {
+                    $startDate = new \DateTime();
+                }
+                if (empty($endDate)) {
+                    $endDate = new \DateTime("2023-01-01");
+                }
+                if (!is_null($isOrganizer)) { // si on veut les sorties dont l'user est l'organisateur, on va chercher son id
+                    // pour l'utiliser avec le Repo
+                    $idOrganizer = $this->getUser()->getId();
+                } else {
+                    $idOrganizer = null;
+                }
+                if (!is_null($isParticipant)) {
+                    $idParticipant = $this->getUser()->getId();
+                } else {
+                    $idParticipant = null;
+                }
+                if (!is_null($notParticipant)) {
+                    $idNotParticipant = $this->getUser()->getId();
+                } else {
+                    $idNotParticipant = null;
+                }
+
+                //TODO supprimer les dump à la prod
+                // On fait un like sur le nom, en ajoutant les % on fait un un LIKE %name%
+                // Cela permet aussi de trouver tout les NightOut ayant certains caractères
+                $nightOutName = "%" . $nightOutName . "%";
+                /**permet de faire une recherche par mots clés*/
+                $nightOutList = $nightOutRepository->selectFilter($campus, $nightOutName, $startDate, $endDate,
+                    $idOrganizer, $idParticipant, $idNotParticipant);
             }
-            if (empty($endDate)) {
-                $endDate = new \DateTime("2023-01-01");
-                dump("On initialise endate");
-            }
-            if(!is_null($isOrganizer)){ // si on veut les sorties dont l'user est l'organisateur, on va chercher son id
-                // pour l'utiliser avec le Repo
-                $idOrganizer = $this->getUser()->getId();
-            } else {$idOrganizer=null;}
-            if(!is_null($isParticipant)){
-                $idParticipant = $this->getUser()->getId();
-            } else {$idParticipant=null;}
-            if(!is_null($notParticipant)){
-                $idNotParticipant = $this->getUser()->getId();
-            }else {$idNotParticipant=null;}
-            dump($campus);
-            dump($nightOutName);
-            dump($isOrganizer);
-            dump($isParticipant);
-            dump($notParticipant);
-            dump($startDate);
-            dump($endDate);
-            dump($idOrganizer);
-            dump($idParticipant);
-            dump($idNotParticipant);
-//TODO supprimer les dump à la prod
-            // On fait un like sur le nom, en ajoutant les % on fait un un LIKE %name%
-            // Cela permet aussi de trouver tout les NightOut ayant certains caractères
-            $nightOutName = "%" . $nightOutName . "%"; /**permet de faire une recherche par mots clés*/
-            $nightOutList = $nightOutRepository->selectFilter($campus, $nightOutName, $startDate, $endDate,
-                $idOrganizer, $idParticipant, $idNotParticipant);
+
         }
-        dump($endDate);
-        dump($startDate);
+
         // Requête permettant de sélectionner tous les articles (avec des inner joins) si le formulaire de filtre
         // n'est pas utilisé
-        dump($nightOutList);
+
         return $this->render('main/index.html.twig',
             compact("nightOutList", "campusList")
         );
