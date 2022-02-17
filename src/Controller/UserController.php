c10c9cadfe999575a514dc699663fa71a7c50979
cgervais2021 <cyrilgervais@hotmail.fr>
2022-02-17 09:38:42 +0100

    modif css

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
@@ -12,54 +10,39 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-
-
-
-
 #[Route("/", name:"")]
 class UserController extends AbstractController
 {
-
-     #[Route("/login", name:"app_login")]
+    #[Route("/login", name:"app_login")]
     public function login(
         AuthenticationUtils $authenticationUtils,
         UserRepository $userRepository,
         EntityManagerInterface $em
-     ): Response
+    ): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-
-
     #[Route("/logout", name:"app_logout")]
     public function logout(): Response
     {
         return $this->redirectToRoute('index');
     }
-
-
-
     /** Gestion du profil */
     #[IsGranted("ROLE_USER")]
     #[Route('/user/{username}', name: 'modify')]
     public function modifierProfil(
         EntityManagerInterface $entityManager,
         Request $request,
-        $username
+                               $username
     ): Response
     {
-
         $user = $this->getUser();
         $form = $this->createForm(UserType::class, $user );
         $form->handleRequest($request);
@@ -67,38 +50,33 @@ class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             $this->addFlash('success', 'Profil mis à jour !');
-
 //            return $this->render('user/detail.html.twig', ['form' =>$form->createView()]);
             return $this->redirectToRoute('detail',
-            ['id'=>$user->getId()]);
+                ['id'=>$user->getId()]);
         }
         return $this->renderForm('modifyProfile/user.html.twig',
             compact('form')
         );
         //TODO faire message erreur si pas marcher
-
     }
-
     /** Affichage d'un profil */
     #[IsGranted("ROLE_USER")]
     #[Route('/user/detail/{id}', name:'detail') ]
     public function detail  (
-    UserRepository $userRepository,
-    $id
+        UserRepository $userRepository,
+                       $id
     ):Response
     {
         $user = $userRepository->find($id);
         dump($user);
         return $this->render('user/detail.html.twig',
-        compact("user")
-    );
+            compact("user")
+        );
     }
-
     #[Route('/loginorregister', name: 'index')]
     public function mainpage(
     ): Response
     {
         return $this->render('user/mainpage.html.twig');
-  }
-
+    }
 }
