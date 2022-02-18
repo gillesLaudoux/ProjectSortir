6ceb3c5d0ed603d590b8ab2360712e13a2a9c283
vretureau2021 <valentin.retureau2021@campus-eni.fr>
2022-02-18 15:18:20 +0100

    Maj RolesUser / Administrator

---
+++ src/Controller/Admin/CityCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\City;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CityCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return City::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            IntegerField::new('zipCode'),
+            AssociationField::new('places')->hideOnForm(),
+        ];
+    }
+
+}
